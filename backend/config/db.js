1993037132768d152bf5bd4e5caa627a48025661
Aditya Dubey <adityadubey412@gmail.com>
2022-04-06 15:11:42 +0530

    mongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = async() =>{
     try {
-        // const conn = await mongoose.connect(process.env.MONGO_URI , {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        // });
-
         const conn = await mongoose.connect(process.env.MONGO_URI , {
             useNewUrlParser: true,
             useUnifiedTopology: true,
