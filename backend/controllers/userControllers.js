b02d3028fe8880f32dfad7bf1cedfed771ba6798
Aditya Dubey <adityadubey412@gmail.com>
2022-04-11 18:10:45 +0530

    pw_encrypt

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,4 +39,23 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await User.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pid: user.pic,
+            token: generateToken(user._id),
+        });
+    }else {
+        res.status(401);
+        throw new Error("Invalid Email or Password");
+    }
+});
+
 module.exports = { registerUser };
